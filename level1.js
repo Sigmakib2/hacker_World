9c7fe89cacb21494d69bd3621f65eeb9dfd4b9a4
Sakib Mahmud <52151316+Sigmakib2@users.noreply.github.com>
2020-10-30 23:47:26 +0600

    Add files via upload

---
+++ level1.js
@@ -0,0 +1,14 @@
+var sms;
+sms="Dr. Hang is disconnected."
+
+function openForm() {
+  document.getElementById("myForm").style.display = "block";
+  
+}
+
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+}
+function sendit() {
+  document.getElementById("demo").innerHTML = sms;
+}
